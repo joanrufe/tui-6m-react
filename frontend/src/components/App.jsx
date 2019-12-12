79ee91d3b475d0f84ddeb518ca7e396c3f1bf91a
Tony <joseantonio.ruizfernandez@tui.be>
2019-12-12 11:39:16 +0100

    Add some extra TUI React Components

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Loader from 'tui-components/lib/atoms/Loader/Loader';
 import Button, { ButtonTypes } from 'tui-components/lib/atoms/Button/Button';
+import Panel from 'tui-components/lib/molecules/Panel/Panel';
 import L10n from '@tuicom/l10n/l10n';
 import translations from '../../l10n/translations.json';
 import './App.scss';
@@ -8,11 +9,14 @@ import './App.scss';
 const App = ({ locale }) => {
   const l10n = new L10n(translations, locale);
   return (
-    <div>
+    <Panel panelTitle="Tui Components" expanded={true}>
       <Loader />
+      <br />
       {l10n.t('Hello World!')}
+      <br />
       <Button type={ButtonTypes.primary}>{l10n.t('React & 6M')}</Button>
-    </div>
+      <br />
+    </Panel>
   );
 };
 
